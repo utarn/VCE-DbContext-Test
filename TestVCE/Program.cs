a86d71f9b728f9b4316184becc87c221ece5190a
Utharn Buranasaksee <utarn@yahoo.com>
2024-03-23 13:57:24 +0700

    Add AsQueryable

--- TestVCE/Program.cs
+++ TestVCE/Program.cs
@@ -18,3 +18,18 @@ var studentsGroupByEmploymentStatusName = students
 watch.Stop();
 var elapsedMs = watch.ElapsedMilliseconds;
 Console.WriteLine($"Time to get all students: {elapsedMs} ms");
+
+
+// measure time to get all students
+watch.Restart();
+
+var studentsQuery = dbContext.GraduatedStudentJobTrackingReportStudentDetails.AsQueryable();
+
+// students count group by EmploymentStatusName
+var studentsGroupByEmploymentStatusNameQuery = studentsQuery
+    .GroupBy(x => x.EmploymentStatusName)
+    .Select(x => new { EmploymentStatusName = x.Key, Count = x.Count() })
+    .ToList();
+watch.Stop();
+var elapsedMs2 = watch.ElapsedMilliseconds;
+Console.WriteLine($"Time to get all students: {elapsedMs2} ms");
